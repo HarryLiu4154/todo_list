22deeb79a7eae7c8205876e59b31618b80685f46
Harry Liu <harryliu4154@hotmail.com>
2025-06-03 18:54:19 -0400

    added basic components for display

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import TodoCard from './TodoCard';
 
 export default function TodoList() {
-  return (
-    <div>TodoList</div>
-  )
+    let todos = [
+        "Gym",
+        "Groceries",
+        "Clean room"
+    ];
+
+    return (
+        <ul className="main">
+            {todos.map((todo, todoIndex) => {
+                return (
+                    <TodoCard key={todoIndex}>
+                        <p>{todo}</p>
+                    </TodoCard>
+                )
+            })}
+        </ul>
+    )
 }
